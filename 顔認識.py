f9055d482f71749f0708553db81648daf7b7f739
fumifumi55 <m1m23r24@oit.ac.jp>
2023-07-22 21:22:27 +0900

    pythonファイルを修正

--- 顔認識.py
+++ 顔認識.py
@@ -1,1 +1,43 @@
-print("1")
+import cv2
+
+# 事前にトレーニング済みの感情分類モデルを読み込む（例：Deep Learningモデル）
+# モデルの読み込みはこの部分を適切に行ってください
+
+# Haar Cascadeの顔検出器を読み込む
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+# カメラを開く
+cap = cv2.VideoCapture(0)
+
+while True:
+    # カメラから画像を取得
+    ret, frame = cap.read()
+
+    # グレースケールに変換
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # 顔の検出
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    for (x, y, w, h) in faces:
+        # 顔の部分を切り取る
+        face_roi = frame[y:y + h, x:x + w]
+
+        # 事前に用意した感情分類モデルを使用して感情を推定する（ここは適切なコードに置き換えてください）
+        # 以下は仮のコードで、"happy"または"neutral"という仮の感情を出力します
+        emotion = "happy"
+
+        # 顔の周囲に枠と感情を表示する
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        cv2.putText(frame, emotion, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+
+    # 画像を表示する
+    cv2.imshow('Emotion Detection', frame)
+
+    # 'q'キーを押して終了
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# カメラをリリースし、ウィンドウを閉じる
+cap.release()
+cv2.destroyAllWindows()
